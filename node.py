066aaa0b16224bf0bedcc5a5eef9f34c8d25418c
chris-cozy <csande9@clemson.edu>
2023-08-04 12:59:17 -0400

    Removed depracated methods

--- node.py
+++ node.py
@@ -6,12 +6,5 @@ class Node:
     def mine(self, block):
         self.blockchain.mine(block, self.node_id)
 
-    def vote_for_blockchain(self, blockchain):
-        return self.blockchain.vote_for_blockchain(blockchain)
-
-    def get_mined_blocks(self):
-        return self.blockchain.mined_blocks
-
-    def update_blockchain(self, new_blocks):
-        for block in new_blocks:
-            self.blockchain.add(block)
+    def set_blockchain(self, blockchain):
+        self.blockchain = blockchain
