ec32be92f1a48941ad770ad9cc3f6f9990c8289e
chris-cozy <csande9@clemson.edu>
2023-08-04 01:23:41 -0400

    Added method for getting the current list of mined blocks, and updating the blockchain

--- node.py
+++ node.py
@@ -8,3 +8,10 @@ class Node:
 
     def vote_for_blockchain(self, blockchain):
         return self.blockchain.vote_for_blockchain(blockchain)
+
+    def get_mined_blocks(self):
+        return self.blockchain.mined_blocks
+
+    def update_blockchain(self, new_blocks):
+        for block in new_blocks:
+            self.blockchain.add(block)
