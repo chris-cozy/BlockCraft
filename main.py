aee9491a7148dac07ca2dec59af55441e3168e7a
chris-cozy <csande9@clemson.edu>
2023-08-04 01:35:43 -0400

    Implemented node array to simulate network

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from block import Block
 from blockchain import Blockchain
 import time
 from node import Node
+import random
 
 
 def main():
@@ -9,35 +10,30 @@ def main():
     Entry point of the blockchain simulator.
     """
     try:
-        node1_blockchain = Blockchain()
-        node1 = Node(node_id=1, blockchain=node1_blockchain)
-        num_blocks_to_mine = 5
+        # Create a network of nodes with their respective blockchains
+        num_nodes = 5
+        nodes = []
+        for i in range(num_nodes):
+            blockchain = Blockchain()
+            node = Node(node_id=i+1, blockchain=blockchain)
+            nodes.append(node)
 
-        for n in range(num_blocks_to_mine):
-            data = f"Node {node1.node_id} - Block {n + 1}"
-            block = Block(data)
-            node1.mine(block)
-
-        # Simulate a network latency for demonstration purposes
-        time.sleep(2)
-
-        node2_blockchain = Blockchain()
-        node2 = Node(node_id=2, blockchain=node2_blockchain)
-
-        for n in range(num_blocks_to_mine):
-            data = f"Node {node2.node_id} - Block {n + 1}"
-            block = Block(data)
-            node1.mine(block)
+        # Mine blocks concurrently
+        num_blocks_to_mine = 10
+        for i in range(num_blocks_to_mine):
+            for node in nodes:
+                data = f"Node {node.node_id} - Block {i}"
+                block = Block(data)
+                node.mine(block)
 
         # Achieve consensus
-        nodes = [node1, node2]
-        node1.blockchain.consensus(nodes)
+        nodes[random.randint(0, len(nodes) - 1)].blockchain.consensus(nodes)
 
         # Print final blockchain for each node
-        print("\nNode 1 blockchain:")
-        node1.blockchain.print_blockchain()
-        print("\nNode 2 blockchain:")
-        node2.blockchain.print_blockchain()
+        print("\nNode blockchains:")
+        for node in nodes:
+            print(f"Node {node.node_id} blockchain:")
+            node.blockchain.print_blockchain()
 
     except ValueError as e:
         print(f"Error {e}")
